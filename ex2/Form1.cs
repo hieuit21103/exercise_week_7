afa819f63bbfef512365abf02201346141a69088
Haruna <hieuit21103@gmail.com>
2023-03-31 11:18:39 +0700

    Update

--- ex2/Form1.cs
+++ ex2/Form1.cs
@@ -15,6 +15,28 @@ namespace ex2
         public Form1()
         {
             InitializeComponent();
+            init();
         }
+
+        public void init()
+        {
+            listView1.Columns.Add("Name");
+            listView1.Columns.Add("Age");
+            listView1.Columns.Add("Gender");
+        }
+
+        private List<Person> people = new List<Person>
+        {
+            new Person { Name = "John", Age = 30, Gender = "Male" },
+            new Person { Name = "Jane", Age = 25, Gender = "Female" },
+            new Person { Name = "Bob", Age = 40, Gender = "Male" }
+        };
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
     }
 }
