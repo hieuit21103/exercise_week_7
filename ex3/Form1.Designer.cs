e2c55660e929488a6f76241209c49c1239d1d17b
Haruna <hieuit21103@gmail.com>
2023-03-31 21:28:16 +0700

    Update

--- ex3/Form1.Designer.cs
+++ ex3/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ex3
+﻿using System.Windows.Forms;
+
+namespace ex3
 {
     partial class Form1
     {
@@ -28,13 +30,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(449, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(467, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 24);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(699, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(89, 24);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(467, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 30);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Chọn màu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button2.Location = new System.Drawing.Point(581, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "B";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button3.Location = new System.Drawing.Point(614, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "I";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.button4.Location = new System.Drawing.Point(647, 42);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(27, 30);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "U";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
 
--- ex3/Form1.cs
+++ ex3/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,165 @@ namespace ex3
         public Form1()
         {
             InitializeComponent();
+            initFont();
+            initDropDown();
         }
+
+        List<string> list = new List<string>();
+        List<int> size = new List<int>()
+        {
+            8,
+            10,
+            12,
+            14,
+            16,
+            18,
+            20,
+            24,
+            28,
+            32,
+            40,
+            48,
+            56,
+            64,
+            72
+        };
+
+        private void initFont()
+        {
+            InstalledFontCollection installedFont = new InstalledFontCollection();
+            FontFamily[] families = installedFont.Families;
+            foreach (FontFamily family in families)
+            {
+                list.Add(family.Name);
+            }
+        }
+        private void initDropDown()
+        {
+            foreach(string font in list)
+            {
+                comboBox1.Items.Add(font);
+            }
+            comboBox1.SelectedIndex = 0;
+            foreach (int size in size)
+            {
+                comboBox2.Items.Add(size);
+            }
+            comboBox2.SelectedIndex= 0;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(richTextBox1.SelectionLength > 0)
+            {
+                Font current = richTextBox1.SelectionFont;
+                if(current != null)
+                {
+                    richTextBox1.SelectionFont = new Font(comboBox1.SelectedItem.ToString(), current.Size, current.Style);
+                }
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(richTextBox1.SelectionLength > 0)
+            {
+                Font current = richTextBox1.SelectionFont;
+                if(current != null)
+                {
+                    richTextBox1.SelectionFont = new Font(current.FontFamily, float.Parse(comboBox2.SelectedItem.ToString()), current.Style);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Color selectedColor = colorDialog1.Color;
+                    richTextBox1.SelectionColor = selectedColor;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (count1 == 0)
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style | FontStyle.Bold;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count1 = 1;
+                }
+            }
+            else
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style & ~FontStyle.Bold;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count1 = 0;
+                }
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (count2 == 0)
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style | FontStyle.Italic;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count2 = 1;
+                }
+            }
+            else
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style & ~FontStyle.Italic;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count2 = 0;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (count3 == 0)
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style | FontStyle.Underline;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count3 = 1;
+                }
+            }
+            else
+            {
+                if (richTextBox1.SelectionLength > 0)
+                {
+                    Font currentFont = richTextBox1.SelectionFont;
+                    FontStyle newFontStyle = currentFont.Style & ~FontStyle.Underline;
+                    richTextBox1.SelectionFont = new Font(currentFont.FontFamily, currentFont.Size, newFontStyle);
+                    count3 = 0;
+                }
+            }
+        }
+
+        private int count1 = 0;
+        private int count2 = 0;
+        private int count3 = 0;
     }
 }
